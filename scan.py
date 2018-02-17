825e5d0bbf5c389e3dbed959b530c764acf15ee6
Conor Maguire <conormag94@gmail.com>
2018-02-17 17:29:06 +0000

    Move scanning into AttendanceTracker class to keep a record of the number of times a number has been recorded

--- scan.py
+++ scan.py
@@ -1,4 +1,7 @@
-from bluepy.btle import Scanner, DefaultDelegate
+from binascii import unhexlify
+
+from bluepy.btle import Scanner, DefaultDelegate, BTLEException
+
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -7,14 +10,35 @@ class ScanDelegate(DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print("Discovered device", dev.addr)
-        elif isNewData:
+        if isNewData:
             print("Received new data from", dev.addr)
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(15.0)
 
-for dev in devices:
-	print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-	for (adtype, desc, value) in dev.getScanData():
-		decoded_data = bytearray.fromhex(value).decode() if value.startswith('192f') else value	
-		print("  %s = %s" % (desc, decoded_data))
+class AttendanceTracker(object):
+    def __init__(self):
+        self.data = dict()
+        self.devices = []
+        self.scanner = Scanner().withDelegate(ScanDelegate())
+
+    def start_scanning(self):
+        try:
+            devices = self.scanner.scan(15.0)
+            for dev in devices:
+                for adtype, desc, value in dev.getScanData():
+                    if adtype == 0x16 and value.startswith('192f'):
+                        decoded_data = unhexlify(value).decode()
+                        if self.data.get(decoded_data):
+                            self.data[decoded_data] += 1
+                        else:
+                            self.data[decoded_data] = 1
+        except BTLEException as e:
+            print("No devices currently broadcasting")
+            print(e)
+
+    def print_data(self):
+        print(self.data)
+
+
+tracker = AttendanceTracker()
+tracker.start_scanning()
+tracker.print_data()
