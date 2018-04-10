859e8a74e7df5c2f9ae6a77cb2a69ec9ad74a063
Conor Maguire <conormag94@gmail.com>
2018-04-10 15:46:49 +0100

    Scheduler now pulls from test endpoint and schedules a lecture based on the time it receives in the response

--- api.py
+++ api.py
@@ -5,6 +5,12 @@ api_url = "http://ec2-54-194-150-186.eu-west-1.compute.amazonaws.com:5001"
 this_room = "LB04"
 
 
+def get_test_schedule():
+    query = api_url + '/test'
+    r = requests.get(query).json()
+    return r['schedule']
+
+
 def get_lectures(room):
     query = api_url + "/schedules/" + room
     r = requests.get(query).json()
@@ -12,7 +18,8 @@ def get_lectures(room):
 
 
 if __name__ == '__main__':
-    lectures = get_lectures(this_room)
+    # lectures = get_lectures(this_room)
+    lectures = get_test_schedule()
     print(len(lectures))
     for lecture in lectures:
         print(lecture["id"], lecture["subject"], lecture["start_time"])
--- attendance.py
+++ attendance.py
@@ -2,25 +2,60 @@ import datetime
 import threading
 import time
 
+import api
 from scan import AttendanceTracker
 
 
-def task(lecture):
-    print("Starting lecture {0} on thread {1}".format(lecture, threading.current_thread()))
-    tracker = AttendanceTracker(lecture)
+this_room = "LB04"
+scheduled_lectures = dict()
+
+
+def start_attendance_tracker(lecture):
+    print("Starting lecture {0} on thread {1}".format(lecture['subject'], threading.current_thread()))
+    tracker = AttendanceTracker(lecture['subject'])
     tracker.record()
-    print("Finished lecture {0}".format(lecture))
+
+    del scheduled_lectures[lecture['id']]
+    print("Finished lecture {0}".format(lecture['subject']))
+
+
+def as_datetime(date_string):
+    date_format = '%a, %d %b %Y %H:%M:%S'
+    return datetime.datetime.strptime(date_string, date_format)
+
+
+def schedule_lecture(lecture_dict):
+    now = datetime.datetime.now()
+    start_time = as_datetime(lecture_dict['start_time'])
+    delay = (start_time - now).total_seconds()
+
+    threading.Timer(delay, start_attendance_tracker, [lecture_dict]).start()
+    scheduled_lectures[lecture_dict['id']] = lecture_dict
+    print("Scheduled Lecture {0} [{1}, {2}] @ {3}".format(
+        lecture_dict['id'], lecture_dict['subject'],
+        lecture_dict['room'], lecture_dict['start_time']
+    ))
+
+
+def update_schedule():
+    schedule = api.get_test_schedule()
+    for lecture in schedule:
+        if scheduled_lectures.get(lecture['id']) is None:
+            schedule_lecture(lecture)
+        else:
+            print("Lecture {0} already scheduled".format(lecture))
 
 
 def start():
-    now = datetime.datetime.now()
-    start_time = now + datetime.timedelta(seconds=4)
-    delay = (start_time - now).total_seconds()
-
-    threading.Timer(delay, task, ["test1"]).start()
-    print("Scheduled lecture, sleeping.... Current thread {0}".format(threading.current_thread()))
+    update_schedule()
+    # now = datetime.datetime.now()
+    # start_time = now + datetime.timedelta(seconds=4)
+    # delay = (start_time - now).total_seconds()
+    #
+    # threading.Timer(delay, start_attendance_tracker, ["test1"]).start()
+    # print("Scheduled lecture, sleeping.... Current thread {0}".format(threading.current_thread()))
     time.sleep(8)
-    print("WOKE FAM")
+    print("Main Thread Woken Up")
 
 
 if __name__ == '__main__':
